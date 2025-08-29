ffc9c5dfdbc549a71b8717c7ec5c33c7612b0871
CarlosM <G00429041@atu.ie>
2025-08-29 18:20:24 +0100

    Book class Finished

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -1,4 +1,18 @@
 package com.example;
 
-public class Book {
+public class Book extends Item {
+    private String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    @Override
+    public String toString() {
+        return "Book [ID=" + id + ", Title=" + title + ", Author=" + author + "]";
+    }
 }
