f7ca9d972fd41bd330dea740f3ef4519d7720808
CarlosM <G00429041@atu.ie>
2025-08-29 18:25:37 +0100

    Item class Finished

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -1,4 +1,15 @@
 package com.example;
 
 public class Item {
+    protected int id;
+    protected String title;
+
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 }
